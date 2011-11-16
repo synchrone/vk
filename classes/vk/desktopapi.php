f90a20a0c50cd9caa15bb13e1d8d50e2f047bc96
Alexander Bogdanov <syn@li.ru>
2011-11-16 17:01:47 +0400

    User auth done & getting token right

--- classes/vk/desktopapi.php
+++ classes/vk/desktopapi.php
@@ -3,26 +3,22 @@
 
 class VK_DesktopApi extends Vk_DocumentedApi{
 
-	/*
-	 *  New format: http://vkontakte.ru/developers.php?oid=-1&p=%D0%9F%D1%80%D0%B0%D0%B2%D0%B0_%D0%B4%D0%BE%D1%81%D1%82%D1%83%D0%BF%D0%B0_%D0%BF%D1%80%D0%B8%D0%BB%D0%BE%D0%B6%D0%B5%D0%BD%D0%B8%D0%B9
-		Код	Описание
-		+1	Пользователь разрешил отправлять ему уведомления.
-		+2	Доступ к друзьям.
-		+4	Доступ к фотографиям.
-		+8	Доступ к аудиозаписям.
-		+16	Доступ к видеозаписям.
-		+32	Доступ к предложениям.
-		+64	Доступ к вопросам.
-		+128	Доступ к wiki-страницам.
-		+256	Добавление ссылки на приложение в меню слева.
-		+512	Добавление ссылки на приложение для быстрой публикации на стенах пользователей.
-		+1024	Доступ к статусам пользователя.
-		+2048	Доступ заметкам пользователя.
-		+4096	(для Desktop-приложений) Доступ к расширенным методам работы с сообщениями.
-		+8192	Доступ к обычным и расширенным методам работы со стеной.
-	 */
     private $accessLevels = array(
-        1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192
+        //'notify', //Пользователь разрешил отправлять ему уведомления.
+        'friends', //Доступ к друзьям.
+        'photos', //Доступ к фотографиям.
+        'audio', //Доступ к аудиозаписям.
+        'video', //Доступ к видеозаписям.
+        'docs', //Доступ к документам.
+        'notes', //Доступ заметкам пользователя.
+        'pages', //Доступ к wiki-страницам.
+        'offers', //Доступ к предложениям (устаревшие методы).
+        'questions', //Доступ к вопросам (устаревшие методы).
+        'wall', //Доступ к обычным и расширенным методам работы со стеной.
+        'groups', //Доступ к группам пользователя.
+        'messages', //(для Standalone-приложений) Доступ к расширенным методам работы с сообщениями.
+        'ads', //Доступ к расширенным методам работы с рекламным API.
+        'offline'	//Доступ к API в любое время со стороннего сервера.
     );
 
     private $appToken;
@@ -63,8 +59,6 @@ class VK_DesktopApi extends Vk_DocumentedApi{
 
     public function __construct(&$config){
 		$this->config = $config;
-        $code = $this->LoginUser($config['user_email'],$config['user_pass']);
-		$this->LoginApp($code);
     }
     public function __wakeup(){
         $this->op_history = array();
@@ -75,6 +69,7 @@ class VK_DesktopApi extends Vk_DocumentedApi{
         }
         return $this->Call($name,count($args) == 1 ? array($args[0]) : $args);
     }
+
     public function Execute($code,$debug=false,$testmode=false){
        if($debug){
            var_dump($code);
@@ -89,27 +84,9 @@ class VK_DesktopApi extends Vk_DocumentedApi{
     }
 
     public function Call($method, array $params = array()){
-        if($this->AppSessionExpired()){
-            $this->LoginApp();
-        }
-		$params['api_id'] = $this->config['app_id'];
-        $params['method'] = $method;
-        //sig
-		if (!isset($params['v'])) $params['v'] = '3.0';
-		if (!isset($params['format']))$params['format'] = 'json';
-
-        //signature gen
-		ksort($params);
-		$sig = '';
-		foreach($params as $k=>$v) {
-			$sig .= $k.'='.$v;
-		}
-		$sig = $this->appCookies['mid'] . $sig . $this->appCookies['secret'];
-
-        $params['sig'] = md5($sig);
-        $params['sid'] = $this->appCookies['sid'];
-
-		$query = $this->curl($this->config['api_url'],$params);
+        $this->LoginApp();
+        $params['access_token'] = $this->appToken['access_token'];
+		$query = $this->curl('https://api.vk.com/method/'.$method,$params);
 		$res = (array)json_decode($query['contents'], true);
 
         if(!isset($res['response'])){
@@ -124,23 +101,26 @@ class VK_DesktopApi extends Vk_DocumentedApi{
                  throw new VK_Exception('Error: '.$res['error']['error_code'].' - '.$res['error']['error_msg'],$e_data);
             }
 
-            throw new Exception('Unknown response');
+            throw new Exception($e_data,'Unknown response');
         }
 		return $res['response'];
-
     }
-    
-    private function LoginUser($email,$password){
+
+    /**
+     * @return string App auth code
+     * @throws Exception
+     */
+    private function LoginUser(){
         //This gonna get us login form, as no cookies provided
-        $login_page = $this->Curl('https://api.vk.com/oauth/authorize',array(
+        $last_request = $this->Curl('https://api.vk.com/oauth/authorize',array(
             'client_id'=>$this->config['app_id'],
             'redirect_uri' => 'blank.html',
             'display' => 'wap',
             'scope' =>  $this->GetFullAccessMask(),
             'response_type'=>'code'
-        ),array(CURLOPT_FOLLOWLOCATION => true,CURLOPT_COOKIEJAR=>true));
+        ),array(CURLOPT_FOLLOWLOCATION => true, CURLOPT_COOKIESESSION => true));
 
-        $nokopage = Nokogiri::fromHtml($login_page['contents']);
+        $nokopage = Nokogiri::fromHtml($last_request['contents']);
         $form = $nokopage->get('form')->toArray();
         $form = $form[0];
 
@@ -148,31 +128,42 @@ class VK_DesktopApi extends Vk_DocumentedApi{
         foreach($nokopage->get('form input')->toArray() as $i){
             $i['name'] = isset($i['name']) ? $i['name'] : '';
             $i['value'] = isset($i['value']) ? $i['value'] : '';
+            if($i['type']=='submit'){continue;}
             $params[$i['name']] = $i['value'];
         }
-        $params['email'] = $email;
-        $params['pass']  = $password;
+        $params['email'] = $this->config['user_email'];
+        $params['pass']  = $this->config['user_pass'];
 
-        $auth_result_form = $this->Curl($form['action'],$params,array(
+        $last_request = $this->Curl($form['action'],$params,array(
             CURLOPT_POST => $form['method'] == 'POST' ? true : false,
-            CURLOPT_COOKIEJAR=>true
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_COOKIE => $last_request['cookie']
         ));
 
-        if(strpos($auth_result_form['contents'],'Login success')===false){
+        if(strpos($last_request['contents'],'Login success')===false){
             //Stage 2: Granting perms
-            $nokopage = Nokogiri::fromHtml($auth_result_form['contents']);
+            $nokopage = Nokogiri::fromHtml($last_request['contents']);
             $form = $nokopage->get('form')->toArray();
+            $form = $form[0];
+            if(substr($form['action'],0,4) != 'http'){$form['action'] = 'http://api.vk.com'.$form['action'];}
+
             $params = array();
             foreach($nokopage->get('form input')->toArray() as $i){
+                $i['name'] = isset($i['name']) ? $i['name'] : '';
+                $i['value'] = isset($i['value']) ? $i['value'] : '';
                 $params[$i['name']] = $i['value'];
             }
 
-            $auth_result_form = $this->Curl($form['action'],$params,array(
-                CURLOPT_POST => $form['method'] == 'POST' ? true : false,
-                CURLOPT_COOKIEJAR=>true
+            $last_request = $this->Curl($form['action'],array(),array(
+                CURLOPT_POST => true, //$form['method'] == 'POST' ? true : false,
+                CURLOPT_FOLLOWLOCATION => false,
+                CURLOPT_COOKIE => $last_request['cookie']
             ));
         }
-        $rurl = $auth_result_form['info']['url'];
+
+
+        $rurl = end($last_request['info']);
+        $rurl=$rurl['url'];
         if(($erroffset = strpos($rurl,'error_description=')) !== false){
             throw new Exception(substr($rurl,urldecode($erroffset)));
         }
@@ -180,66 +171,52 @@ class VK_DesktopApi extends Vk_DocumentedApi{
         return $code[1];
     }
 
-    private function LoginApp(){
-        $loginResult = $this->curl($this->config['applogin_url'],
-            array(
-                'app'=>$this->config['app_id'],
-                'layout'=>'popup',
-                'type'=>'browser',
-                'settings'=>$this->GetFullAccessMask()
-            ),
-            array(
-                CURLOPT_FOLLOWLOCATION => true,
-                CURLOPT_COOKIE =>$this->getUserCookieStr()
-            )
-        );
-
-        if(strstr($loginResult['info']['url'],'login.php')){
-            throw new VK_Exception("User must add proxy app and allow interaction",$loginResult['contents']);
-            /* needs figurin out
-            $hash = preg_match("/var auth_hash = '([0-9a-fA-F]{18})'/",$loginResult['contents'],$loginHash);
-            var_dump($hash);die();
-            $this->curl($this->loginUrl,array(),array(
-                CURLOPT_POSTFIELDS => $this->params(array(
-                    'act'=>'',
-                    'app'=>$this->appId,
-                    'hash'=>'',
-                    'permanent'=>'1'
-                ))
-            ));*/
+    /**
+     * @param string $code App auth code returned by user
+     * @return void
+     * @throws Exception
+     */
+    private function LoginApp($code = null){
+        if($this->appToken !== null && //authd
+                $this->appToken['access_token'] !== null && //got a token
+                ($this->appToken['expire_time'] != -1 || //infinite
+                $this->appToken['expire_time'] < time()) //or not expired
+        ){
+            return;
         }
-
-        if(strstr($loginResult['info']['url'],'login_fail')){
-            throw new Exception('Wrong User Cookie');
+        if($code === null){
+            $code = $this->LoginUser();
         }
-        
-        $loginResponse = explode('session=',$loginResult['info']['url']);
-        if(!isset($loginResponse[1])){
-            ob_start();
-            var_dump($loginResult);
-            throw new Exception('Unexpected response:\n'.ob_get_clean());
+        $token = $this->Curl('https://api.vk.com/oauth/access_token',array(
+            'client_id' => $this->config['app_id'],
+            'client_secret' => $this->config['app_secret'],
+            'code' => $code
+        ));
+        $token = json_decode($token['contents'],true);
+        if(isset($token['error'])){
+            throw new Exception($token['error_description'],$token['error']);
         }
-        $this->appCookies =  (array)json_decode(urldecode($loginResponse[1]));
+        if($token['expires_in'] > 0){ $token['expire_time'] = time() + $token['expires_in']; }else{
+            $token['expire_time'] = -1;
+        }
+        $this->appToken = $token;
     }
 
     private function GetFullAccessMask(){
-        $sum = 0;
-        foreach($this->accessLevels as $l){
-            $sum+=$l;
-        }
-        return $sum;
-    }
-    private function AppSessionExpired(){
-        return $this->appCookies==null || ($this->appCookies['expire'] < time() && $this->appCookies['expire'] > 0);
+        return implode(',',$this->accessLevels);
     }
 
-    protected function Curl($address, $params, $options = null){
+    protected function Curl($address, $params = null, $options = null,$redirect_count=0){
+        if(isset($options[CURLOPT_MAXREDIRS]) && $redirect_count > $options[CURLOPT_MAXREDIRS]){
+            throw new Exception('Max redirect count exceeded');
+        }
+
         if(isset($options[CURLOPT_POST]) && $options[CURLOPT_POST] === true){
             $options[CURLOPT_POSTFIELDS] = $params;
             $params = array();
         }
 
-        $address .= '?'.$this->Params($params);
+        if($params != null && !empty($params)){ $address .= '?'.$this->Params($params);}
 
         //now just init that
         $c = curl_init($address);
@@ -248,19 +225,21 @@ class VK_DesktopApi extends Vk_DocumentedApi{
         $defaultOptions = array(
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_AUTOREFERER => true,
-            CURLOPT_FOLLOWLOCATION => true,
-            CURLINFO_HEADER_OUT => true
+            CURLINFO_HEADER_OUT => true,
+            CURLOPT_HEADER => true
         );
         foreach($defaultOptions as $k=>$v){
             if(!isset($options[$k])){
                 $options[$k] = $v;
             }
         }
+        if(isset($options[CURLOPT_HTTPHEADER]) && is_array($options[CURLOPT_HTTPHEADER])){//possibly check on existence
+            $options[CURLOPT_HTTPHEADER][] = 'Expect:';
+        }else{
+            $options[CURLOPT_HTTPHEADER]= array('Expect:');
+        }
+        $options[CURLOPT_FOLLOWLOCATION]=false;//gonna handle that myself
         foreach($options as $k=>$v){
-            if(($k == CURLOPT_COOKIEJAR || $k == CURLOPT_COOKIEFILE ) && $v == true){
-                $v = '/tmp/curl_cookies';
-                $options[CURLOPT_COOKIEJAR] = $v;
-            }
             curl_setopt($c,$k,$v);
         }
 
@@ -277,8 +256,83 @@ class VK_DesktopApi extends Vk_DocumentedApi{
                 throw new Exception("Cookie was demanded, but can't retrive from curl file");
             }
         }
-        $resp = array('info'=>$info,'contents'=>$return);
-        $this->op_history[] = $address;//array('request'=>$address,'response'=>$resp);
+
+        if(isset($options[CURLOPT_HEADER]) && $options[CURLOPT_HEADER]===true){
+            $set_cookie = array();
+            $headers = substr($return,0,$info['header_size']);
+            $return = substr($return,$info['header_size']);
+
+            $responses_headers = explode("\r\n\r\n",trim($headers));
+            foreach($responses_headers as &$headers){
+                $str_headers = explode("\r\n",$headers);
+                $headers = array();
+                foreach($str_headers as $header){
+                    $header = explode(': ',$header);
+                    if(count($header) == 2){
+                        $headers[$header[0]] = $header[1];
+
+                        if($header[0]=='Set-Cookie'){
+                            $set_cookie[] = $header[1];
+                        }
+                    }else if(strpos($header[0],'HTTP')!==false){
+                        $headers['HTTP'] = $header[0];
+                    }
+                }
+            }
+            $info['response_header'] = $responses_headers;
+
+
+            $cookies = array();
+            //covering the cookies we had
+            if(isset($options[CURLOPT_COOKIE])){
+                foreach(explode('; ',$options[CURLOPT_COOKIE]) as $cookie){
+                    $cookie = explode('=',$cookie);
+                    $cookies[$cookie[0]] = $cookie[1];
+                }
+            }
+            //overwriting with what we got
+            foreach($set_cookie as $cookie){
+                $cookie = explode('; ',$cookie);
+                $cookie = explode('=',$cookie[0]);
+                $cookies[$cookie[0]] = $cookie[1];
+            }
+
+            $str_cookies = array();
+            foreach($cookies as $n=>$v){
+                $str_cookies[] = $n.'='.$v; //then re-collapsing back to header string
+            }
+            $info['cookie'] = implode('; ',$str_cookies);
+
+
+            if(isset($info['response_header'][0]['Location']))//doing a recursive redirect
+            {
+                $options[CURLOPT_REFERER]=$info['url'];
+                $options[CURLOPT_COOKIE]=$info['cookie'];
+                unset($options[CURLOPT_POST]);
+                unset($options[CURLOPT_POSTFIELDS]);
+
+                if(substr($info['response_header'][0]['Location'],0,4)!=='http'){
+                    $url = parse_url($address);
+                    if(substr($info['response_header'][0]['Location'],0,1)=='/'){
+                        $path = $info['response_header'][0]['Location'];
+                    }else{
+                        $path=pathinfo($address,PATHINFO_DIRNAME).'/'.$info['response_header'][0]['Location'];
+                    }
+                    $info['response_header'][0]['Location']=sprintf(
+                        '%s://%s%s%s',
+                        $url['scheme'],$url['host'],(isset($url['port'])? ':'.$url['port'] : ''),$path
+                    );
+                }
+                $return = $this->Curl($info['response_header'][0]['Location'],array(),$options,++$redirect_count);
+                array_unshift($return['info'],$info);
+                $info=$return['info'];
+                $return = $return['contents'];
+            }else{
+                $info = array($info);
+            }
+        }
+        $resp = array('info'=>$info,'contents'=>$return,'cookie'=>$info[count($info)-1]['cookie']);
+        $this->op_history[] = $address; //array('request'=>$address,'response'=>$resp);
         return $resp;
     }
 	
