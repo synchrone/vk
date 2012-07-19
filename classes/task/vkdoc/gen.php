dd9e25ed2fa49fe28c3251d6df0208f4a302399f
Alexander Bogdanov <syn@li.ru>
2012-07-19 21:24:26 +0400

    Generating docs plus returnvalues

--- classes/task/vkdoc/gen.php
+++ classes/task/vkdoc/gen.php
@@ -9,8 +9,7 @@
 class Task_VKDoc_Gen extends Minion_Task
 {
     protected $_options = array(
-        'config' => 'default',
-        'filename' => null
+        'config' => 'default'
     );
 
     public function _execute(array $params){
@@ -19,11 +18,22 @@ class Task_VKDoc_Gen extends Minion_Task
         VKDoc::$config = $params['config'];
 
         $doc = new VKDoc();
-        $class_contents = Kohana::FILE_SECURITY.$doc->generate();
 
-        $filename = $params['filename'] !== null ? $params['filename'] : Kohana::find_file('classes/vk','documentedapi');
-        if($filename===null){$filename=MODPATH.'vk/classes/vk/documentedapi.php';}
-
+        $class_contents = Kohana::FILE_SECURITY."\n".$doc->generate();
+        $filename=MODPATH.'vk/classes/vkdoc/api/full.php';
         file_put_contents($filename,$class_contents);
+
+        $values_path = MODPATH.'vk/classes/vkdoc/returnvalue/';
+        foreach($doc->get_methods() as /** @var $val VKDoc_Method*/ $val)
+        {
+            $safename = str_replace('_',DIRECTORY_SEPARATOR,strtolower($val->get_safename()));
+            $filename = $values_path.$safename.EXT;
+            $dirname = dirname($filename);
+            if(!file_exists($dirname))
+            {
+                mkdir($dirname,0755,true);
+            }
+            file_put_contents($filename, Kohana::FILE_SECURITY."\n".$val->get_return_class());
+        }
     }
 }
