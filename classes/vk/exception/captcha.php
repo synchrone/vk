28e2bc031df69d4ede07f9744d0e0885e0312fa3
Alexander Bogdanov <syn@li.ru>
2012-07-19 21:25:15 +0400

    VK_Exception_Captcha

---
+++ classes/vk/exception/captcha.php
@@ -0,0 +1,40 @@
+<?php
+class VK_Exception_Captcha extends VK_Exception {
+    protected $request_info;
+    private $error;
+
+    public function __construct($request_info,$error_array) {
+        parent::__construct($error_array['error_msg'],$error_array['error_code']);
+        $this->error = $error_array;
+        $this->request_info = $request_info;
+    }
+
+    public function __toString(){
+        $msg = 'A captcha response query is needed: '.
+            sprintf('%s&captcha_sid=%s&captcha_key=<human typed text from %s>',
+                $this->getFailedCallUrl(),
+                $this->getSid(),
+                $this->getImgURL()
+            );
+        return $msg;
+    }
+
+    public function getFailedCallUrl(){
+        $params = array();
+        foreach($this->error['request_params'] as $param){
+            if($param['key'] == 'method'){continue;}
+            $params[] = sprintf('%s=%s',urlencode($param['key']),urlencode($param['value']));
+        }
+        $url = $this->request_info[count($this->request_info)-1]['url'].'?'.
+            implode('&',$params);
+
+        return $url;
+    }
+
+    public function getSid(){
+        return $this->error['captcha_sid'];
+    }
+    public function getImgURL(){
+        return $this->error['captcha_img'];
+    }
+}
