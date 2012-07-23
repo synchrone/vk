55930d04043df05bdd7ae125dd0ed2d86e90d0a9
Alexander Bogdanov <syn@li.ru>
2012-07-23 21:16:26 +0400

    Removing old style auth

--- init.php
+++ init.php
@@ -1,10 +1,3 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-
-Route::set('vk/xd_receiver.htm', 'xd_receiver.htm')
-	->defaults(array(
-		'controller' => 'vk',
-		'action'     => 'xd_receiver'
-	));
-
 ?>
