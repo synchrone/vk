652018ca7d8b4bd2ad0a26762fdde818bb7d9f68
Alexander Bogdanov <syn@li.ru>
2012-07-24 21:09:38 +0400

    Using --return-types=true for generating dummy return type classes

--- classes/task/vkdoc/gen.php
+++ classes/task/vkdoc/gen.php
@@ -11,6 +11,7 @@ class Task_VKDoc_Gen extends Minion_Task
     protected $_options = array(
         'config' => 'default',
         'lang'   => 'ru',
+        'return-types' => 'false',
     );
 
     public function _execute(array $params){
@@ -20,23 +21,25 @@ class Task_VKDoc_Gen extends Minion_Task
         VKDoc::$language = $params['lang'];
 
         $doc = new VKDoc();
-
         $class_contents = Kohana::FILE_SECURITY."\n".$doc->generate();
+        
         $filename=MODPATH.'vk/classes/vkdoc/api/full.php';
         file_put_contents($filename,$class_contents);
 
-        //Notice: this code generates return value dummy classes without @property hint PHPDocs.
-//        $values_path = MODPATH.'vk/classes/vkdoc/returnvalue/';
-//        foreach($doc->get_methods() as /** @var $val VKDoc_Method*/ $val)
-//        {
-//            $safename = str_replace('_',DIRECTORY_SEPARATOR,strtolower($val->get_safename()));
-//            $filename = $values_path.$safename.EXT;
-//            $dirname = dirname($filename);
-//            if(!file_exists($dirname))
-//            {
-//                mkdir($dirname,0755,true);
-//            }
-//            file_put_contents($filename, Kohana::FILE_SECURITY."\n\n".$val->get_return_class());
-//        }
+        if($params['return-types']=='true'){
+            //Notice: this code generates return value dummy classes without @property hint PHPDocs.
+            $values_path = MODPATH.'vk/classes/vkdoc/returnvalue/';
+            foreach($doc->get_methods() as /** @var $val VKDoc_Method*/ $val)
+            {
+                $safename = str_replace('_',DIRECTORY_SEPARATOR,strtolower($val->get_safename()));
+                $filename = $values_path.$safename.EXT;
+                $dirname = dirname($filename);
+                if(!file_exists($dirname))
+                {
+                    mkdir($dirname,0755,true);
+                }
+                file_put_contents($filename, Kohana::FILE_SECURITY."\n\n".$val->get_return_class());
+            }
+        }
     }
 }
